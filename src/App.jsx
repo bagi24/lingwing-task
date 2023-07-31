021b0f08e5558c89877a7d8c678dd5125ddca103
unknown <bagrat.injgia24@gmail.com>
2023-07-31 12:56:24 +0400

    create Content Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./component/Header";
+import Content from "./component/Content";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Content />
     </div>
   );
 }
---
+++ src/component/Content.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import EnFlag from "../images/eng_flag.png"
+
+
+const Content = () => {
+  return (
+    <div className='content'>
+        <div className='content-header'>
+        <h1 className='h1'>დაიწყე <strong className='h1-1'> უცხო ენაზე </strong>  საუბარი დღესვე </h1>
+        </div>
+    
+
+<div className='content-languages'>
+<div className='en-box'> 
+
+<img className='en-Flag' src={EnFlag} alt="" />
+
+ </div>
+
+
+</div>
+
+    </div>
+  )
+}
+
+export default Content
